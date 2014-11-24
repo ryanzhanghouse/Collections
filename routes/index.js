e91df59aff2cbab0c1df9c15c3390a2da520b662
Ryan Zhang <ryanzhanghouse@gmail.com>
2014-11-24 21:29:30 +0800

    ejs

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'My collections' });
+  res.render('index');
 })
 
 router.get('/book', function(req, res) {
-  res.render('book', { title: 'Books I read' });
+  res.render('book');
 });
 
 router.get('/movie', function(req, res) {
-  res.render('movie', { title: 'Movies I watched' });
+  res.render('movie');
 });
 
 router.get('/music', function(req, res) {
-  res.render('music', { title: 'Music I love' });
+  res.render('music');
 });
 
 router.get('/about', function(req, res) {
-  res.render('about', { title: 'About me' });
+  res.render('about');
 });
 
 module.exports = router;
